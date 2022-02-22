d499ad2d06a8c6130c8d04704dc006979e8bc541
Walter Nóbrega <77422570+JNOR010@users.noreply.github.com>
2022-02-22 13:21:12 -0300

    Lista de exercicios

---
+++ Curso_de_Python/Execercio_de_treino.py
@@ -0,0 +1,25 @@
+'''
+#Ex. 01
+def saudacao(cumprimento, nome):
+    return cumprimento + nome
+
+cp = saudacao('Ola ', 'Walter')
+print(cp)
+'''
+'''
+#Ex. 02
+def conta(n1,n2,n3):
+    return n1 + n2 + n3
+
+soma = conta(3,5,6)
+print(soma)
+'''
+'''
+#Ex. 03
+
+def cal(n1, n2):
+    return ((n1/100)*n2) + n1
+
+conta = cal(50,10)
+print(conta)
+'''
