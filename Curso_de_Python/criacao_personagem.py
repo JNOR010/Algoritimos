e2babcf8cd8a84f83e3239254501bee7dca7666c
Walter Nóbrega <77422570+JNOR010@users.noreply.github.com>
2022-03-31 13:18:25 -0300

    criacao_personagem01

---
+++ Curso_de_Python/criacao_personagem.py
@@ -0,0 +1,29 @@
+print('Olá, Bem vindo ao Mundo de Drømmer. Utilize o menu a seguir para criar o seu personagem')
+
+class personagem():
+    def __init__(self, nome, idade, sexo):
+        self.nome = nome
+        self.idade = idade
+        self.sexo = sexo
+
+nome = input('Bravo sonhador qual o seu nome:')
+idade = input('Sonha a quanto tempo:')
+sexo = input('Em seu sonho, qual o seu genero?:')
+
+class arma():
+    def __init__(self, armamento, ataque):
+        self.armamento = armamento
+        self.ataque = ataque
+
+armamento = input('Qual sua especialidade? Espada, Cajado ou Punhos: ')
+if armamento == Espada or espada:
+    ataque = Slash
+if armamento == Cajado or cajado:
+    ataque = Magico
+elif armamento == Punhos or punho:
+    ataque = Fisico
+
+
+class equipamento():
+    def __init__(self) -> None:
+        pass
